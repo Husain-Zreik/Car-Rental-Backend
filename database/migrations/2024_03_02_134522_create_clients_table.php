09e61d5104a36620b5282a565fbd9ed3ca9aae82
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2024-03-02 17:17:23 +0200

    create clients table

--- database/migrations/2024_03_02_134522_create_clients_table.php
+++ database/migrations/2024_03_02_134522_create_clients_table.php
@@ -6,20 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('number');
+            $table->string('address');
+            $table->string('front_image_url');
+            $table->string('back_image_url')->nullable();
             $table->timestamps();
+
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('sponsor_id')->nullable()->constrained('sponsors')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('clients');
