082d2185c13b9eae01b1068cef9074ed06c637d1
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2024-03-02 19:08:33 +0200

    add validation for register and login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -25,48 +26,67 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        $request->validate([
-            'username' => 'required|string|max:255',
-            'password' => 'required|string|min:6',
-        ]);
+        try {
+            $request->validate([
+                'username' => 'required|string|max:255|unique:users',
+                'password' => 'required|string|min:6',
+            ],);
 
-        $user = User::create([
-            'username' => $request->username,
-            'password' => Hash::make($request->password),
-        ]);
+            $user = User::create([
+                'username' => $request->username,
+                'password' => Hash::make($request->password),
+            ]);
 
-        $token = Auth::login($user);
-        $user->token = $token;
+            $token = Auth::login($user);
+            $user->token = $token;
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'User created successfully',
-            'user' => $user,
-        ]);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User created successfully',
+                'user' => $user,
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Validation failed',
+                'errors' => $e->errors(),
+            ], 422);
+        };
     }
 
     public function login(Request $request)
     {
-        $request->validate([
-            'username' => 'required|string',
-            'password' => 'required|string',
-        ]);
-        $credentials = $request->only('username', 'password');
+        try {
+            $request->validate([
+                'username' => 'required|string',
+                'password' => 'required|string',
+            ], [
+                'username.required' => 'The username field is required.',
+                'password.required' => 'The password field is required.',
+            ]);
+            $credentials = $request->only('username', 'password');
 
-        $token = Auth::attempt($credentials);
-        if (!$token) {
+            $token = Auth::attempt($credentials);
+            if (!$token) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Unauthorized',
+                ], 401);
+            }
+
+            $user = Auth::user();
+            $user->token = $token;
+            return response()->json([
+                'status' => 'success',
+                'user' => $user,
+            ]);
+        } catch (ValidationException $e) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Unauthorized',
-            ], 401);
-        }
-
-        $user = Auth::user();
-        $user->token = $token;
-        return response()->json([
-            'status' => 'success',
-            'user' => $user,
-        ]);
+                'message' => 'Validation failed',
+                'errors' => $e->errors(),
+            ], 422);
+        };
     }
 
     public function logout()
