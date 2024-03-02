4748280c778ed53ce76a1794a89ef6814050db37
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2024-03-02 17:17:38 +0200

    create cars table

--- database/migrations/2024_03_02_134730_create_cars_table.php
+++ database/migrations/2024_03_02_134730_create_cars_table.php
@@ -6,14 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            $table->string('image_url');
+            $table->string('name');
+            $table->string('plate');
+            $table->string('model');
             $table->timestamps();
+
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
         });
     }
 
