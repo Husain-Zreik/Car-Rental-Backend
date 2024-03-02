7d10118512e0c9cfe3915a78aa439e6c68c4125d
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2024-03-02 17:18:29 +0200

    create transactions table

--- database/migrations/2024_03_02_134731_create_transactions_table.php
+++ database/migrations/2024_03_02_134731_create_transactions_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->string('description');
+            $table->decimal('amount', 8, 2);
+            $table->enum('type', ['expense', 'income']);
             $table->timestamps();
+
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('transactions');
