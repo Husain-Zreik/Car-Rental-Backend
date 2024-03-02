c35d8d177dfd88a0d44a629b7c6feceffad2a288
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2024-03-02 19:53:28 +0200

    create getSponsors route

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,16 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'guest'], function () {
-
     Route::get('unauthorized', [AuthController::class, 'unauthorized'])->name("unauthorized");
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 });
 
 Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function () {
-    Route::post('sponsor/add', [SponsorController::class, 'addSponsor']);
+
+    Route::get('sponsors/display', [SponsorController::class, 'getSponsors']);
+    Route::post('sponsors/add', [SponsorController::class, 'addSponsor']);
+
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
 });
