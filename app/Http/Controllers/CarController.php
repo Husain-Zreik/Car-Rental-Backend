71ed72b3a4a6bf08f054e3d66bbbc4f0dad674f7
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2024-03-03 17:59:01 +0200

    edit getCars function

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Car;
+use App\Models\Client;
+use App\Models\Rental;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
@@ -51,14 +53,38 @@ class CarController extends Controller
 
     public function getCars()
     {
-        $user = Auth::user();
+        $cars = Car::where('user_id', Auth::id())->get();
 
-        $cars = Car::where('user_id', $user->id)->get();
+        $carsData = [];
+        foreach ($cars as $car) {
+
+            $rental = Rental::where('car_id', $car->id)->orderBy('start_date', 'desc')->first();
+
+            if ($rental) {
+                $client = Client::find($rental->client_id);
+            }
+
+            $imagePath = asset('storage/' . $car->image_path);
+
+            $carsData[] = [
+                'id' => $car->id,
+                'name' => $car->name,
+                'plate' => $car->plate,
+                'model' => $car->model,
+                'image_path' => $imagePath,
+                'available_status' => $car->available_status ? True : False,
+                'renting_info' => $car->available_status ? 'not rented' : [
+                    'client_name' => $client->name,
+                    'start_date' => $rental->start_date,
+                    'end_date' => $rental->end_date,
+                ],
+            ];
+        }
 
         return response()->json([
             'status' => 'success',
             'message' => 'cars retrieved successfully',
-            'cars' => $cars,
+            'cars' => $carsData,
         ]);
     }
 }
